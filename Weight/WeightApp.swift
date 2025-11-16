dd4a0929690145fd785f5b5377292d282f3bd6f9
Training1 <cwood@anbank.com>
2025-11-16 10:52:48 -0600

    Initial Commit

---
+++ Weight/WeightApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightApp.swift
+//  Weight
+//
+//  Created by Chris Wood on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct WeightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeightTests/WeightTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeightTests.swift
+//  WeightTests
+//
+//  Created by Chris Wood on 11/16/25.
+//
+
+import Testing
+@testable import Weight
+
+struct WeightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
